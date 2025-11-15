d7af617cdf184845eccb344ca558cef33dc89e2e
Sean Clendening <sean@firstascentdesign.com>
2025-11-15 13:50:36 -0500

    Add basic template rendering support
    Add Thymeleaf dependency and configure pico CSS framework

--- capstone/src/main/java/com/example/capstone/CapstoneController.java
+++ capstone/src/main/java/com/example/capstone/CapstoneController.java
@@ -1,15 +1,13 @@
 package com.seanclen.capstone;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import static org.springframework.http.HttpStatus.OK;
 
 @Controller
 public class CapstoneController {
     @GetMapping("/")
-    ResponseEntity<String> Message() {
-        return new ResponseEntity<>("CS-499 Capstone", OK);
+    public String home(Model model) {
+        return "index";
     }
 }
